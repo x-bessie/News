fa6552ce4ccd0b061092a4806987c213f90bd602
x-bessie <qwas002@126.com>
2018-12-24 15:55:44 +0800

    添加新闻API获取后的类

--- src/main/java/entity/Classifiy.java
+++ src/main/java/entity/Classifiy.java
@@ -1,4 +1,68 @@
 package entity;
 
+/**
+ *聚合新闻api
+ */
 public class Classifiy {
+//    新闻发布源
+    public String author_name;
+//    新闻标题
+    public String title;
+//    新闻链接
+    public String url;
+//    新闻分类
+    public String category;
+//    新闻发布时间
+    public String date;
+
+    public String getAuthor_name() {
+        return author_name;
+    }
+
+    public void setAuthor_name(String author_name) {
+        this.author_name = author_name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Classifiy{" +
+                "author_name='" + author_name + '\'' +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                ", category='" + category + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
 }
--- src/main/java/entity/Everything.java
+++ src/main/java/entity/Everything.java
@@ -1,4 +1,59 @@
 package entity;
 
+/**
+ * Newsapi 的everything请求
+ */
 public class Everything {
+//    发布作者
+    public  String author;
+//    新闻标题
+    public  String title;
+//    新闻链接
+    public  String url;
+//    新闻发布时间
+    public  String publishedAt;
+//    新闻图片
+    public  String urlToImage;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Everything{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                ", publishedAt='" + publishedAt + '\'' +
+                '}';
+    }
 }
